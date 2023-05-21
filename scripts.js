5ccc2c2f72eb1e751785a0362e3afd67c227ac10
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-05-21 20:22:10 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,133 @@
+let number1  = document.getElementById("1")
+let number2  = document.getElementById("2")
+let number3  = document.getElementById("3")
+let number4  = document.getElementById("4")
+let number5  = document.getElementById("5")
+let number6  = document.getElementById("6")
+let number7  = document.getElementById("7")
+let number8  = document.getElementById("8")
+let number9  = document.getElementById("9")
+let number10 = document.getElementById("10")
+let number11 = document.getElementById("11")
+let number12 = document.getElementById("12")
+let number13 = document.getElementById("13")
+let number14 = document.getElementById("14")
+let number15 = document.getElementById("15")
+let number16 = document.getElementById("16")
+let number17 = document.getElementById("17")
+let number18 = document.getElementById("18")
+let number19 = document.getElementById("1")
+let number20 = document.getElementById("20")
+let number21 = document.getElementById("21")
+let number22 = document.getElementById("22")
+let number23 = document.getElementById("23")
+let number24 = document.getElementById("24")
+let number25 = document.getElementById("25")
+let number26 = document.getElementById("26")
+let number27 = document.getElementById("27")
+let number28 = document.getElementById("28")
+let number29 = document.getElementById("29")
+let number30 = document.getElementById("30")
+let number31 = document.getElementById("31")
+let number32 = document.getElementById("32")
+let number33 = document.getElementById("33")
+let number34 = document.getElementById("34")
+let number35 = document.getElementById("35")
+let number36 = document.getElementById("36")
+let number37 = document.getElementById("37")
+let number38 = document.getElementById("38")
+let number39 = document.getElementById("39")
+let number40 = document.getElementById("40")
+let number41 = document.getElementById("41")
+let number42 = document.getElementById("42")
+let number43 = document.getElementById("43")
+let number44 = document.getElementById("44")
+let number45 = document.getElementById("45")
+let number46 = document.getElementById("46")
+let number47 = document.getElementById("47")
+let number48 = document.getElementById("48")
+let number49 = document.getElementById("49")
+let number50 = document.getElementById("50")
+let number51 = document.getElementById("51")
+let number52 = document.getElementById("52")
+let number53 = document.getElementById("53")
+let number54 = document.getElementById("54")
+let number55 = document.getElementById("55")
+let number56 = document.getElementById("56")
+let number57 = document.getElementById("57")
+let number58 = document.getElementById("58")
+let number59 = document.getElementById("59")
+let number60 = document.getElementById("60")
+
+number1.addEventListener("click", pegaBtns)
+number2.addEventListener("click", pegaBtns)
+number3.addEventListener("click", pegaBtns)
+number4.addEventListener("click", pegaBtns)
+number5.addEventListener("click", pegaBtns)
+number6.addEventListener("click", pegaBtns)
+
+function pegaBtns() {
+    if (number1.click) {
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+    if (number1.click) {
+        alert("marcou")
+        number1.classList.add('chk')
+    }
+}
